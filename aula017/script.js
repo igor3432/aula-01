e4e1d6c5b44130e56b1971a8b633fe6cedc9b198
igor3432 <137342044+igor3432@users.noreply.github.com>
2024-09-10 21:17:17 -0300

    Estrutura de repetição

---
+++ aula017/script.js
@@ -0,0 +1,34 @@
+console.log('vai começar...')
+for(var c = 1;c <= 4;c++){
+    console.log(c)
+
+}console.log('fim!')
+
+
+
+
+
+
+/*for(var c = 1;c <= 15;c++){
+    console.log(c)
+}*/
+
+
+
+
+/*var c = 1 
+do {
+    console.log(c)
+    c++
+}while (c <= 5)*/
+
+
+
+
+
+
+/*var c = 1 
+while (c <= 5) { 
+    console.log(c)
+    c++
+}*/
